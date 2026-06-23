90fd5e303712b4d0f24b4a8586bb0cf9b90790bd
Jae Hyun <betive37@gmail.com>
2026-06-23 21:07:58 +0900

    Set version to 0.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = resolvedTargetSdk
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "0.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
